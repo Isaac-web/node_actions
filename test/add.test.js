8df8cb4cc8e3b678c2d1da122cd5e5ea3c34f843
Isaac-web <kanytakiy@gmail.com>
2024-02-28 09:48:53 +0000

    Most Tests

--- test/add.test.js
+++ test/add.test.js
@@ -5,4 +5,12 @@ describe('Add', () => {
     const result = add(1, 1);
     expect(result).toBe(2);
   });
+  it('should return 10 for 5 + 5', () => {
+    const result = add(5, 5);
+    expect(result).toBe(10);
+  });
+  it('should return 0 for -1 + 1', () => {
+    const result = add(-1, 1);
+    expect(result).toBe(0);
+  });
 });
